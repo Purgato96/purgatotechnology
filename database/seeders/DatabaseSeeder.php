8359cfc56fccbb61d7c5ab12dc3c60c6f726c3c4
Purgato96 <matheus_purgato@hotmail.com.com>
2025-03-07 16:47:25 -0300

    Blog listagem, adição, exclusão, edição, paginação OK

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder {
     public function run(): void {
         $this->call([
             UserSeeder::class,
-            PostSeeder::class,
+          //  PostSeeder::class,
         ]);
         //User::factory(10)->create();
 
