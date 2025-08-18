48cca7b4a8f1ea3538a7c5a4c0e91c94c9c72551
Purgato96 <matheus_purgato@hotmail.com.com>
2025-08-18 14:10:53 -0300

    Correção de cache

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\NoCacheForInertia;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
            HandleInertiaRequests::class,
+            NoCacheForInertia::class,
+
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
