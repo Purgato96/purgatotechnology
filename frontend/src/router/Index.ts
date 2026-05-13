981bcec3704e08380348494efe527854597f1d51
Purgato96 <matheus_purgato@hotmail.com>
2026-05-13 12:17:24 -0300

    alter: Fixing files

--- frontend/src/router/Index.ts
+++ frontend/src/router/Index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Index from '@/views/Index.vue'
+import Index from '@/views/IndexPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
